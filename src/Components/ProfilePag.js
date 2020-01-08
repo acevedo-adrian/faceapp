72b3e7e688a1853ebe195dc7b4742f446fe77856
Acevedo Adrian <adrian@MacBook-Pro-de-Acevedo.local>
2020-01-08 16:51:15 -0300

    cambios descartadors

--- src/Components/ProfilePag.js
+++ src/Components/ProfilePag.js
@@ -1,16 +1,71 @@
-import React, { Component } from "react";
+import React, { Fragment } from "react";
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import { withStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
 
-class ProfilePag extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      users: props.users
-    };
-  }
-  render() {
-    return <h1>Desde return</h1>;
-  }
+import Typography from '@material-ui/core/Typography';
+
+
+import Container from '@material-ui/core/Container';
+
+
+
+const ProfilePag = (props) => {
+
+    
+    // const userSelect = users.filter((user)=>{
+    //   if(user.login.username === "blueelephant921"){
+    //     return user
+    //   }      
+    // })
+    
+    return(
+    )
+  
 }
 
-export default ProfilePag;
+export default withStyles({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+    },
+    li: {
+      listStyle: 'none',
+    },
+  },
+ 
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: (1, 1.5),
+  },
+  heroContent: {
+    padding: (8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:'dark' 
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: (2),
+  },
+  footer: {
+    borderTop: `1px solid `,
+    marginTop: (8),
+    paddingTop: (3),
+    paddingBottom: (3),
+    
+  },
+})(ProfilePag);
